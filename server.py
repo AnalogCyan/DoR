2fe75b0648926c3fc15a9b14c3c6d2b19b569e62
AnalogCyan <git@thayn.me>
2023-03-27 10:51:33 -0500

    Refactor to create local cache of valid IPs.

--- server.py
+++ server.py
@@ -2,37 +2,45 @@ import os
 import socket
 import logging
 import time
+import json
+import random
 from dnslib import DNSRecord, RR, A, QTYPE
 from dnslib.server import DNSServer, BaseResolver
 import requests
 from concurrent.futures import ThreadPoolExecutor
+from ip_generator import main as generate_ip_data
+import pathlib
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
 class RandomIPResolver(BaseResolver):
-    def __init__(self, num_workers=10):
+    def __init__(self, num_workers=10, json_file="ip_data.json"):
         self.executor = ThreadPoolExecutor(max_workers=num_workers)
+        self.json_file = json_file
+        self._load_ip_data()
 
-    def is_valid_ip(self, ip):
-        try:
-            response = requests.get(f'http://{ip}', timeout=5)
-            return response.status_code == 200
-        except requests.exceptions.RequestException:
-            return False
+    def _load_ip_data(self):
+        if self._should_generate_ip_data():
+            print("Generating IP data...")
+            ip_range = "192.168.1.0/24"
+            generate_ip_data(ip_range, self.json_file)
 
-    def find_valid_ip(self):
-        valid_ip = None
-        while not valid_ip:
-            ips = [
-                f'{os.urandom(1)[0]}.{os.urandom(1)[0]}.{os.urandom(1)[0]}.{os.urandom(1)[0]}' for _ in range(10)]
-            results = list(self.executor.map(self.is_valid_ip, ips))
-            for ip, is_valid in zip(ips, results):
-                if is_valid:
-                    valid_ip = ip
-                    break
-        return valid_ip
+        with open(self.json_file, "r") as f:
+            self.ip_data = json.load(f)
+
+    def _should_generate_ip_data(self):
+        if not pathlib.Path(self.json_file).exists():
+            return True
+
+        modification_time = pathlib.Path(self.json_file).stat().st_mtime
+        age_in_days = (time.time() - modification_time) / (60 * 60 * 24)
+
+        return age_in_days > 30
+
+    def get_random_ip(self):
+        return random.choice(list(self.ip_data.keys()))
 
     def resolve(self, request, handler):
         reply = request.reply()
@@ -40,10 +48,10 @@ class RandomIPResolver(BaseResolver):
         if q.qtype == QTYPE.A:
             logger.debug('Resolving IP address for query: %s', q.qname)
             start_time = time.time()
-            ip = self.find_valid_ip()
+            ip = self.get_random_ip()
             elapsed_time = time.time() - start_time
             logger.debug(
-                'Found valid IP: %s (elapsed time: %.2f seconds)', ip, elapsed_time)
+                'Found random IP: %s (elapsed time: %.2f seconds)', ip, elapsed_time)
             reply.add_answer(
                 RR(rname=q.qname, rtype=QTYPE.A, rdata=A(ip), ttl=60))
         return reply
